1d6912f51dd1198e1b96cb66012cb834a8c3abd5
Eleazar Meza <bairesdev_emeza@iseatz.com>
2020-12-15 18:17:13 -0400

    Simplified Search model method and argument naming

--- app/models/search.rb
+++ app/models/search.rb
@@ -20,45 +20,45 @@ class Search
   def results
     return nil unless valid?
 
-    engines_results = case engine
+    responses = case engine
                       when 'both'
                         [GoogleSearch.call(text), BingSearch.call(text)]
                       else
                         ["#{engine.capitalize}Search".constantize.call(text)]
                       end
 
-    self.class.agreggate_engines_results(engines_results)
+    self.class.agreggate_responses(responses)
   end
 
-  def self.agreggate_engines_results(engines_results)
+  def self.agreggate_responses(responses)
     {
-      status: aggregate_engines_statuses(engines_results),
-      status_by_provider: aggregate_providers_statuses(engines_results),
-      results: aggregate_engines_results(engines_results)
+      status: aggregate_statuses(responses),
+      results: aggregate_results(responses),
+      status_by_provider: status_by_provider(responses),
     }
   end
 
-  def self.aggregate_engines_statuses(engines_results)
-    engines_results.any? { |engine_results| engine_results[:status] == :ok } ? :ok : :service_unavailable
+  def self.aggregate_statuses(responses)
+    responses.any? { |response| response[:status] == :ok } ? :ok : :service_unavailable
   end
 
-  def self.aggregate_providers_statuses(engines_results)
-    engines_results.map do |engine_results|
+  def self.status_by_provider(responses)
+    responses.map do |response|
       {
-        provider: engine_results[:provider],
-        status: engine_results[:status],
-        error_messages: engine_results[:error_messages]
+        provider: response[:provider],
+        status: response[:status],
+        error_messages: response[:error_messages]
       }
     end
   end
 
-  def self.aggregate_engines_results(engines_results)
-    head, *tail = engines_results.map do |engine_results|
-      engine_results[:data].map do |results|
+  def self.aggregate_results(responses)
+    head, *tail = responses.map do |response|
+      response[:data].map do |result|
         {
-          provider: engine_results[:provider],
-          title: results[:title],
-          link: results[:link]
+          provider: response[:provider],
+          title: result[:title],
+          link: result[:link]
         }
       end
     end
